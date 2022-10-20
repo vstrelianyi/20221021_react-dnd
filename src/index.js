65e8e69200f4570b149bc9fc79371bf612017f30
Viktor Strelianyi <chmpain@gmail.com>
2022-10-21 02:02:22 +0300

    added cool dnd functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "the-new-css-reset/css/reset.css"
+import initialData from './data';
+import Column from './column';
+import { DragDropContext } from 'react-beautiful-dnd';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const App = () => {
+	const [ data, setData ] = useState( initialData );
+
+	const handleDragEnd = result => {
+		const { destination, source, draggableId } = result;
+		if( !destination ) return;
+		if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+		const column = data.columns[source.droppableId ];
+		const newTaskIds = [...column.taskIds];
+		newTaskIds.splice( source.index, 1 );
+		newTaskIds.splice( destination.index, 0, draggableId );
+
+		const newColumn = {
+			...column,
+			taskIds: newTaskIds
+		}
+
+		const newState = {
+			...data,
+			columns: {
+				...data.columns,
+				[newColumn.id]: newColumn
+			}
+		}
+
+		setData( newState );
+	}
+
+	return(
+		<DragDropContext onDragEnd={ handleDragEnd }
+		>
+			{ data.columnOrder.map( columnId => {
+				const column = data.columns[columnId ];
+				const tasks = column.taskIds.map( taskId => data.tasks[taskId] )
+				return ( <Column key={column.title} column={ column } tasks={ tasks }/> );
+			}) }
+		</DragDropContext>
+	)
+};
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App/>);
